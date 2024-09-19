1050b002a4976b5509dff8328d8cc175497985e3
subh <harvirbuttar2917@gmail.com>
2024-09-19 11:30:03 -0700

    [Backend | Io-socket Events] wrote all the event handlers of our sockets in here

---
+++ apps/backend/src/services/socket/events/handlers.ts
@@ -0,0 +1,142 @@
+/**
+ * Byimaan
+ */
+
+import { Message } from "@prisma/client";
+
+import { Socket, Server as IoServer } from "socket.io";
+import { CLIENT_EVENTS } from ".";
+
+export type ConversationBaseProps = {
+    conversationId: string
+};
+
+export type ConversationUserBaseProps = ConversationBaseProps & {
+    userId: string
+}
+
+export type ConversationParticipantBaseProps = ConversationBaseProps & {
+    participantId: string
+}
+
+export type HandleMessageProps =  {
+    conversationId ?: string
+    message: Omit<Message, "updatedAt" | "deletedBy" >,
+}
+
+export type UserTypingProps = ConversationUserBaseProps & ConversationParticipantBaseProps
+
+export type IsUserOnlineProps = {
+    userId: string;
+}
+
+/** ------------ Events to send everybody in the room ------------ */
+
+const handleSendMessageToConversation = (socket: Socket, props:HandleMessageProps) => {
+    const conversationId = props?.conversationId ?? props.message.conversationId;
+    if (typeof conversationId === 'string') {
+        /** not broadcast send to all participant including sender according to the frontend logic*/
+        socket.to(conversationId).emit(CLIENT_EVENTS.RECEIVE_MESSAGE_FROM_CONVERSATION, props);
+    };
+};
+
+
+/** ------------ Events to broadcast ------------ */
+
+const handleUserIsTypingInConversation = (socket: Socket, props: UserTypingProps) => {
+    if (typeof props.conversationId === 'string') {
+        socket.broadcast.to(props.conversationId).emit (CLIENT_EVENTS.SOMEONE_IS_TYPING_IN_CONVERSATION ,
+            {
+                ...props,
+                // @ts-ignore
+                userId: socket?.userId ?? props.userId
+            }
+        );
+    };
+};
+
+
+const handleUserStoppedTypingInConversation = (socket: Socket, props: UserTypingProps) => {
+    if (typeof props.conversationId === 'string') {
+        socket.broadcast.to(props.conversationId).emit (CLIENT_EVENTS.SOMEONE_HAS_STOPPED_TYPING_IN_CONVERSATION , props);
+    };
+};
+
+
+/** ------------ Events to send only to the user (socket) ------------ */
+
+const handleDeleteMessageFromConversation = (socket: Socket, props: HandleMessageProps) => {
+    const conversationId = props?.conversationId ?? props.message.conversationId;
+    if (typeof conversationId === 'string') {
+        /** not broadcast just send to all participant including sender according to the frontend logic*/
+        socket.emit(CLIENT_EVENTS.YOU_HAVE_DELETED_A_MESSAGE_FROM_CONVERSATION, props);
+    };
+};
+
+const handleJoinConversation = (socket: Socket, {conversationId}: ConversationBaseProps) => {
+    if (typeof conversationId === 'string' && !socket.rooms.has(conversationId)) {
+        socket.join(conversationId);
+        socket.emit(CLIENT_EVENTS.YOU_HAVE_JOINED_CONVERSATION, {conversationId} )
+    };
+};
+
+const handleJoinUserRoom = (socket: Socket, io: IoServer, {userId}: Omit<ConversationUserBaseProps, 'conversationId'>) => {
+    /** userId is the name of the room */
+    //@ts-ignore
+    const isItUserItself = socket?.userId === userId, roomName = userId;
+    const roomExists = io.sockets.adapter.rooms.has(roomName);
+
+    /** This user himself can't intentionally join his own room. Upon socket connection this is backend's responsibilty to automatically join him in his own room */
+    /** Outsiders can only join this room if this rrom exists */
+    if (typeof userId === 'string' && !isItUserItself && !socket.rooms.has(roomName) && roomExists ) {
+        socket.join(userId);
+        socket.emit(CLIENT_EVENTS.YOU_HAVE_JOINED_USER_ROOM, {userId} );
+        socket.emit(CLIENT_EVENTS.YOUR_REQUESTED_USER_IS_ONLINE, {userId} );
+    } else {
+        socket.emit(CLIENT_EVENTS.YOUR_REQUESTED_USER_IS_OFFLINE, {userId} );
+    }
+};
+
+const handleIsUserOnline = (socket: Socket, io: IoServer, {userId}: IsUserOnlineProps) => {
+    const roomIdAsUserId = userId;
+    if (typeof roomIdAsUserId === 'string') {
+        const room  = io.sockets.adapter.rooms.get(roomIdAsUserId);
+        if (room) {
+            socket.emit(CLIENT_EVENTS.YOUR_REQUESTED_USER_IS_ONLINE, {userId});
+            //@ts-ignore
+            const userNeedToJoinThisRoom = !room.has(socket.id) && roomIdAsUserId !== socket?.userId;
+            if (userNeedToJoinThisRoom){
+                socket.join(roomIdAsUserId);
+                socket.emit(CLIENT_EVENTS.YOU_HAVE_JOINED_USER_ROOM, {userId});
+            }
+        } else {
+            /** If no room is found, it means that the user is offline */
+            socket.emit(CLIENT_EVENTS.YOUR_REQUESTED_USER_IS_OFFLINE, {userId});
+        }
+    } 
+};
+
+
+const handleLeaveUserRoom = (socket: Socket, {userId}: Omit<ConversationUserBaseProps, 'conversationId'>) => {
+    
+    const roomIdAsUserId = userId;
+    //@ts-ignore
+    const isItUserItself = socket?.userId === userId;
+
+    if (typeof roomIdAsUserId === 'string' && socket.rooms.has(roomIdAsUserId) && !isItUserItself) {
+        socket.leave(roomIdAsUserId);
+        socket.emit(CLIENT_EVENTS.YOU_HAVE_LEFT_USER_ROOM, {userId} );
+    }
+}
+
+export const socketEventhandlers = {
+    handleSendMessageToConversation,
+    handleUserIsTypingInConversation,
+    handleUserStoppedTypingInConversation,
+    handleDeleteMessageFromConversation,
+    handleJoinConversation,
+    handleJoinUserRoom,
+    handleIsUserOnline,
+    handleLeaveUserRoom
+};
+
---
+++ apps/backend/src/services/socket/events/index.ts
@@ -0,0 +1,175 @@
+/**
+ * Byimaan
+ * 
+ * All socket.io events names that server will handle
+ */
+
+import { Socket, Server as IoServer } from "socket.io";
+import { socketEventhandlers } from "./handlers";
+import { 
+    ConversationBaseProps,
+    ConversationUserBaseProps,
+    HandleMessageProps,
+    IsUserOnlineProps,
+    UserTypingProps
+ } from "./handlers";
+
+const {
+    handleSendMessageToConversation,    
+    handleUserIsTypingInConversation,
+    handleUserStoppedTypingInConversation,
+    handleDeleteMessageFromConversation,
+    handleJoinConversation,
+    handleJoinUserRoom,
+    handleIsUserOnline,
+    handleLeaveUserRoom
+} = socketEventhandlers;
+
+/** The events that will be received from client */
+export const EVENTS = {
+    JOIN_CONVERSATION : "join_conversation",
+
+    /** it is possible for a user with id u123 to join the room of user with id u456 does not matter whether he is online or not  */
+    JOIN_USER_ROOM : "join_user_room",
+
+    LEAVE_USER_ROOM : "leave_user_room",
+
+    SEND_MESSAGE_TO_CONVERSATION : "send_message_to_conversation",
+    DELETE_MESSAGE_FROM_CONVERSATION : "delete_message_from_conversation",
+    USER_IS_TYPING_IN_CONVERSATION : "user_is_typing_in_conversation",
+    USER_STOPPED_TYPING_IN_CONVERSATION : "user_stopped_typing_in_conversation",
+
+    /* Client can make special request to check if someone is online */
+    IS_USER_ONLINE : "is_user_online",
+
+} as const;
+
+
+/** Events that will be sent from server to client */
+export const CLIENT_EVENTS = {
+    RECEIVE_MESSAGE_FROM_CONVERSATION : "receive_message_from_conversation",
+    
+    /**
+     * Clue: client event that starts with YOU or YOUR means only the user will receive the event
+     */
+
+    YOU_HAVE_JOINED_CONVERSATION : "joined_conversation",
+    YOU_HAVE_DELETED_A_MESSAGE_FROM_CONVERSATION : "you_have_deleted_a_message_from_conversation",
+
+    YOU_HAVE_JOINED_USER_ROOM : "you_have_joined_user_room",
+    YOU_HAVE_LEFT_USER_ROOM : "you_have_left_user_room",
+
+    YOUR_REQUESTED_USER_IS_ONLINE : "your_requested_user_is_online",
+    YOUR_REQUESTED_USER_IS_OFFLINE : "your_requested_user_is_offline",
+
+    /** The following event is not yet implemented but still can be used in the future if any new feature is needed to implement or to use this as solution to a problem  */
+    YOU_HAVE_TO_LEAVE_USER_ROOM : "you_have_to_leave_this_room",
+
+    /** ERROR */
+    YOU_NOT_GAVE_USER_ID_FOR_CONNECTION : "you_not_gave_user_id_for_connection",
+
+    /** Clue : Any client events satrts with SOMEONE will be broadcasted means user itself will not receive the event */
+    SOMEONE_IS_TYPING_IN_CONVERSATION : "someone_is_typing_in_conversation",
+    SOMEONE_HAS_STOPPED_TYPING_IN_CONVERSATION : "someone_has_stopped_typing_in_conversation",
+    SOMEONE_IS_ONLINE : "someone_is_online",
+    SOMEONE_IS_OFFLINE : "someone_is_offline",
+
+} as const;
+
+
+const onConnection = (socket: Socket, io : IoServer) => {
+
+    /** Remember after connection it is important to join this socket to the group of 
+     *  <socket.userId> and emit the event  to that group which indicates that this someone who they care about is online
+     * 
+     *  # Again upon we need to dispatch the event indicating that someone who they care about is offline
+     */
+
+    /** Join this user in a room named after his userId */
+    //@ts-ignore
+    if (socket?.userId && typeof socket?.userId === 'string') {
+        //@ts-ignore
+        const userRoomId = socket?.userId as string;
+
+        socket.join(userRoomId);
+        socket.broadcast.to(userRoomId).emit(CLIENT_EVENTS.SOMEONE_IS_ONLINE, {userId : userRoomId});
+    };
+
+    /** ^^^^^^^^^^^ SOCKET EVENTS HANDLERS ^^^^^^^^^^^ */
+
+
+    /** ------------ Events to send only to the user (socket) ------------ */
+
+
+    socket.on(
+        EVENTS.JOIN_CONVERSATION,
+        (props: ConversationBaseProps) => handleJoinConversation(socket, props)
+    );
+
+    socket.on(
+        EVENTS.JOIN_USER_ROOM,
+        (props: Omit<ConversationUserBaseProps, 'conversationId'>) => handleJoinUserRoom(socket, io, props)
+    );
+    
+    socket.on(EVENTS.DELETE_MESSAGE_FROM_CONVERSATION, 
+        (props: HandleMessageProps) => handleDeleteMessageFromConversation(socket, props)
+    );
+
+    socket.on(
+        EVENTS.IS_USER_ONLINE,
+        (props: IsUserOnlineProps) => handleIsUserOnline(socket, io, props)
+    );
+
+    socket.on(
+        EVENTS.LEAVE_USER_ROOM,
+        (props: Omit<ConversationUserBaseProps, 'conversationId'>) => handleLeaveUserRoom(socket, props)
+    );
+
+    /** ------------ Events to broadcast ------------ */
+
+
+    socket.on(
+        EVENTS.USER_IS_TYPING_IN_CONVERSATION,
+        (props: UserTypingProps) => handleUserIsTypingInConversation(socket, props)
+    );
+
+    socket.on(
+        EVENTS.USER_STOPPED_TYPING_IN_CONVERSATION,
+        (props: UserTypingProps) => handleUserStoppedTypingInConversation(socket, props)
+    );
+
+    
+    /** ------------ Events to send everybody in the room ------------ */
+
+    socket.on(
+        EVENTS.SEND_MESSAGE_TO_CONVERSATION,
+        (props: HandleMessageProps) => handleSendMessageToConversation(socket, props)
+    )
+
+};
+const onDisconnect = (socket: Socket, _io : IoServer) => {
+
+    //@ts-ignore
+    if (socket?.userId && typeof socket?.userId === 'string') {
+        //@ts-ignore
+        const userRoomId = socket?.userId as string;
+
+        /** Let the other user know that the user is going to offline */
+        socket.broadcast.to(userRoomId).emit(CLIENT_EVENTS.SOMEONE_IS_OFFLINE, {userId : userRoomId});
+        socket.leave(userRoomId);
+
+        /** Since the if main user has left his room, So at the client side we will implement a event of `LEAVE_USER_ROOM` so that room can get empty */
+
+        /** Just for consistency we still dispatch YOU_HAVE_TO_LEAVE_USER_ROOM */
+
+        socket.broadcast.to(userRoomId).emit(
+            CLIENT_EVENTS.YOU_HAVE_TO_LEAVE_USER_ROOM,
+            {userId : userRoomId}
+        )
+    };
+};
+
+export {
+    onConnection,
+    onDisconnect
+};
