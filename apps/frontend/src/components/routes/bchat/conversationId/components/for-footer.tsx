ed33f739d91a2f1b48a4ff6a2a24d08afa61ab89
subh <harvirbuttar2917@gmail.com>
2024-09-27 11:58:57 -0700

    [Typing | socket event] added the user is typing... feature in conversation. which will be used in both conversation header and app sidebar

--- apps/frontend/src/components/routes/bchat/conversationId/components/for-footer.tsx
+++ apps/frontend/src/components/routes/bchat/conversationId/components/for-footer.tsx
@@ -17,7 +17,6 @@ import { HtmlAllMimeTypes } from "@/constants/file-type";
 import { Message } from "@repo/db";
 import { useSocketEvents } from "@/providers/io-socket/SocketProvider";
 import toast from "react-hot-toast";
-import { set } from "react-hook-form";
 
 const DEFAULT_CONTENT_TYPE: HtmlAllMimeTypes = "text/plain"
 
@@ -37,14 +36,17 @@ export function ChatInput({className, myParticipantId, myUserId, conversationId}
 
     const disable = text.trim().length === 0;
     
-    const {dispatchSendMessageToConversation} = useSocketEvents()
+    const {
+      dispatchSendMessageToConversation,
+      dispatchIamTypingInConversation,
+      dispatchIHaveStoppedTypingInConversation
+    } = useSocketEvents()
 
     const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
       setText(e.target.value);
     };
 
     const dispatchMessage = (contentType: HtmlAllMimeTypes=DEFAULT_CONTENT_TYPE) => {
-        const toastMsg = toast.loading("Sending message ...")
         const messageToDispatch : Message = {
           id: uuidV4(),
           conversationId,
@@ -55,11 +57,6 @@ export function ChatInput({className, myParticipantId, myUserId, conversationId}
           url: null,
           contentType,
         };
-
-        
-        setTimeout(() => {
-          toast.dismiss(toastMsg);
-        }, 1000);
         
         dispatchSendMessageToConversation({
           conversationId,
@@ -68,10 +65,30 @@ export function ChatInput({className, myParticipantId, myUserId, conversationId}
         setText('');
     };
 
+    const handleImTyping = () => {
+      dispatchIamTypingInConversation({
+        conversationId,
+        participantId: myParticipantId,
+        userId: myUserId
+      })
+    };
+
+    const handleImNoLongerTyping = () => {
+      dispatchIHaveStoppedTypingInConversation({
+        conversationId,
+        participantId: myParticipantId,
+        userId: myUserId
+      })
+    }
+
     // Adjust height based on content
     useEffect(() => {
       if (textareaRef.current) {
+        /** If it was not set to `auto` then height will always stick to the previous height  */
         textareaRef.current.style.height = "auto"; 
+        /** After setting to `auto` then height will be adjusted and have got the opportunity to update the scrollHeight.
+         * Now this is time to fix its height back to the scrollHeight not no longer `auto`
+         */
         textareaRef.current.style.height = `${textareaRef.current.scrollHeight}px`; 
       }
     }, [text]); 
@@ -79,7 +96,17 @@ export function ChatInput({className, myParticipantId, myUserId, conversationId}
 
     return (
       <div className={cn("flex justify-center items-end text-base bg-gray-200 rounded-xl w-fit", className)}>
-          <textarea className="w pl-1 w-[280px] xs:w-[360px] md:w-[460px]  outline-none bg-transparent text-sm resize-none max-h-[220px] overflow-y-scroll scrollbar-none self-center " rows={1} placeholder="Type a message" ref={textareaRef} value={text} onChange={handleChange} />
+          <textarea 
+            className="w pl-1 w-[280px] xs:w-[360px] md:w-[460px]  outline-none bg-transparent text-sm resize-none max-h-[220px] overflow-y-scroll scrollbar-none self-center "
+            rows={1}
+            placeholder="Type a message" 
+            ref={textareaRef}
+            value={text}
+            onChange={handleChange}
+
+            onFocus={handleImTyping}
+            onBlur={handleImNoLongerTyping}
+          />
           <button disabled={disable} onClick={() => dispatchMessage()} type="button" className="text-white bg-primary-bchat rounded-full flex justify-center items-center">
               <MdArrowOutward className="m-2"/>
           </button>
