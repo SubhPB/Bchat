a5d6690a6f4f3f72488bf00d2ed6d5aafee571c7
subh <harvirbuttar2917@gmail.com>
2024-09-30 00:20:31 -0700

    [sub-components] for createChatGroup

---
+++ apps/frontend/src/components/routes/bchat/createChatGroup/index.tsx
@@ -0,0 +1,17 @@
+/** Byimaan */
+
+import React from 'react'
+
+function index() {
+  return (
+    <div>
+        {
+            /**
+             * All logic will be written here
+             */
+        }
+    </div>
+  )
+}
+
+export default index
