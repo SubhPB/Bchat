3fbc519be4e4d10802b89689f9e443241a37d1a4
subh <harvirbuttar2917@gmail.com>
2024-10-05 12:14:39 -0700

    [Refactored | Css | Captialized text] Fixed the CSS problems of skeleton and made minor UI changes

--- apps/frontend/src/components/routes/bchat/createChatGroup/card/index.tsx
+++ apps/frontend/src/components/routes/bchat/createChatGroup/card/index.tsx
@@ -7,6 +7,7 @@ import {CustomImgAvatar} from '@/components/common/custom-img-avatar';
 import { Skeleton as ShadCnSkeleton } from '@/components/ui/skeleton';
 import { Card, CardContent } from '@/components/ui/card';
 import { cn } from '@/lib/utils';
+import { captializeText } from '@/utils/features/typing/text';
 
 type Props = {
     contact: ContactSucceesReturnType['GET'][0];
@@ -37,19 +38,19 @@ function SelectCard({contact, className, isSelected, onSelect}: Props) {
                     isAwsObject
                 />
                 <div className='space-y-2 text-xs'>
-                    <p className="font-bold text-primary-bchat w-[90%] text-sm truncate capitalize">
-                        {contact.name ?? contactUser.name}
+                    <p className="font-bold text-primary-bchat w-[90%] text-sm truncate ">
+                        {captializeText(contact.name ?? contactUser.name)}
                     </p>
                     {
                         !contactNameEqualsUserName && (
                             <p className='text-secondary-bchat
-                            w-[90%] truncate capitalize'>
-                                {contactUser.name}
+                            w-[90%] truncate '>
+                                {captializeText(contactUser.name)}
                             </p>
                         )
                     }
                     <p className='text-secondary-bchat
-                        w-[90%] truncate capitalize'>{contactUser.email}
+                        w-[90%] truncate '>{contactUser.email}
                     </p>
                 </div>
             </CardContent>
@@ -60,10 +61,10 @@ function SelectCard({contact, className, isSelected, onSelect}: Props) {
 export function SelectCardSkeleton(){
     const Skeleton = ({className}: Pick<Props, "className">) => <ShadCnSkeleton className={cn("bg-gray-300", className)}/>
     return (
-      <Card className="w-full p-2">
+      <Card className="w-full inline-block mr-1 mb-1 p-2 max-w-[220px]">
         {/* Will adjust later on... */}
         <CardContent className="gap-2 p-0">
-          <Skeleton className="w-full"></Skeleton>
+          <Skeleton className="w-full aspect-[1.3] p-2 rounded-sm mb-1"></Skeleton>
           <div className=" space-y-3 text-[1rem]">
             <Skeleton className="w-[80%] h-6 rounded-lg"/>
             <Skeleton className="w-[65%] h-4 rounded-lg"/>
