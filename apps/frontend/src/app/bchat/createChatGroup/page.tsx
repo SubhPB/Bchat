9126a166dbc16987b4853d74c636728d951c7e05
subh <harvirbuttar2917@gmail.com>
2024-10-08 13:46:42 -0700

    [Added FileContextProvider to keep track of selected group image]

--- apps/frontend/src/app/bchat/createChatGroup/page.tsx
+++ apps/frontend/src/app/bchat/createChatGroup/page.tsx
@@ -8,8 +8,10 @@ import { RenderPropHOC } from '@/components/render_props';
 import { Workarea } from '@/components/routes/bchat/layout/workarea';
 
 import InitData from '@/components/routes/bchat/createChatGroup';
+import { FileContextProvider } from '@/providers/file';
 
 import { GroupNameField, ParticipantsField } from '@/components/routes/bchat/createChatGroup/form/fields';
+import GroupImage from '@/components/routes/bchat/createChatGroup/form/group-img';
 
 import Infobar from '@/components/common/Infobar';
 import ChatGroupForm from '@/components/routes/bchat/createChatGroup/form';
@@ -39,104 +41,116 @@ function page() {
              
             }
 
-            <RenderPropHOC.ARRAY<SelectedContacts[number]>
-              defaultState={[]}>
-              {
-                ({
-                  state: selected, 
-                  setState: setSelection
-                }) => (
-                  
-                  <div className='w-full max-w-[790px]'>
-                    <ChatGroupForm
-                      submitAtAPIEndpoint={apiEndpointToSubmitForm}
-                      selectedContacts={selected}
-                      setSelectedContacts={setSelection}
-                    >
-                      {
-                        (form) => (
-                          <>
-                            <GroupNameField 
-                              form={form}
-                              isLoading={form.formState.isSubmitting}
-                              className='w-full'
-                            />
-                            {
+            {/* 
+              For the GroupImage this context will keep record of the actual file image 
+              & set useByRenderProps to true to use it with renderProps pattern
+             */}
+            <FileContextProvider useByRenderProps={false}>
+
+              <RenderPropHOC.ARRAY<SelectedContacts[number]>
+                defaultState={[]}
+                >
+                {
+                  ({
+                    state: selected, 
+                    setState: setSelection
+                  }) => (
+                    
+                    <div className='w-full max-w-[790px]'>
+                      <ChatGroupForm
+                        submitAtAPIEndpoint={apiEndpointToSubmitForm}
+                        selectedContacts={selected}
+                        setSelectedContacts={setSelection}
+                      >
+                        {
+                          (form) => (
+                            <>
+                              <GroupNameField 
+                                form={form}
+                                isLoading={form.formState.isSubmitting}
+                                className='w-full'
+                              />
+
+                              <div className='columns-[300px]'>
+                                <GroupImage form={form} className='break-inside-avoid max-w-[300px]'/>
                                 <ParticipantsField
-                                  className='w-full flex rounded-lg border-2 border-secondary-bchat flex-wrap gap-2 p-3'
+                                  className='w-full flex rounded-lg border-2 border-secondary-bchat flex-wrap gap-2 p-3 break-inside-avoid'
                                   form={form}
                                   isLoading={form.formState.isSubmitting}
                                   selectedContacts={selected}
                                   setSelectedContacts={setSelection}
                                 />
-                            }
-                          </>
-                        )
-                      }
-                    </ChatGroupForm>
+                              </div>
+                            </>
+                          )
+                        }
+                      </ChatGroupForm>
 
 
-                    {
-                      // Inform the user that no user is selected
-                      selected.length === 0 && (
-                        <Infobar warning renderCloseButton className='transition-all'> Please Select at least one user to create group </Infobar>
-                      )
-                    }
-
-                    <InitData>
-                      {/* Remember InitData will handle the loading by itself */}
                       {
-                        (data) => (
-                          <ContactsHandler
-                            contacts={data}
-                            renderSearchbar
-                            className={''}
-                            ChildSkeleton={SelectCardSkeleton}
-                          >
-                            {
-                              (filteredData) => (
-                                <div className='columns-[220px] gap-1 space-y-2'>
-                                  {
-                                    filteredData.map(
-                                      contact => {
-
-                                        const isSelected = selected.some(
-                                          s => s.contact.id === contact.contact.id
-                                        );
-
-                                        const onSelect = () =>{
-
-                                          if (isSelected){
-                                            setSelection(prev => prev.filter(c => c.contact.id !== contact.contact.id))
-                                          } else {
-                                            setSelection(prev => [...prev, contact])
-                                          }
-                                        }
-
-                                        return(
-                                          
-                                              <SelectCard 
-                                                className='break-inside-avoid'
-                                                key={contact.id}
-                                                contact={contact}
-                                                isSelected={isSelected}
-                                                onSelect={onSelect}
-                                              />
-                                        )
-                                      }
-                                    )
-                                  }
-                                </div>
-                              )
-                            }
-                          </ContactsHandler>
+                        // Inform the user that no user is selected
+                        selected.length === 0 && (
+                          <Infobar warning renderCloseButton className='transition-all'> Please Select at least one user to create group </Infobar>
                         )
                       }
-                    </InitData>
-                  </div>
-                )
-              }
-            </RenderPropHOC.ARRAY>  
+
+                      <InitData>
+                        {/* Remember InitData will handle the loading by itself */}
+                        {
+                          (data) => (
+                            <ContactsHandler
+                              contacts={data}
+                              renderSearchbar
+                              className={''}
+                              ChildSkeleton={SelectCardSkeleton}
+                            >
+                              {
+                                (filteredData) => (
+                                  <div className='columns-[220px] gap-1 space-y-2'>
+                                    {
+                                      filteredData.map(
+                                        contact => {
+
+                                          const isSelected = selected.some(
+                                            s => s.contact.id === contact.contact.id
+                                          );
+
+                                          const onSelect = () =>{
+
+                                            if (isSelected){
+                                              setSelection(prev => prev.filter(c => c.contact.id !== contact.contact.id))
+                                            } else {
+                                              setSelection(prev => [...prev, contact])
+                                            }
+                                          }
+
+                                          return(
+                                            
+                                                <SelectCard 
+                                                  className='break-inside-avoid'
+                                                  key={contact.id}
+                                                  contact={contact}
+                                                  isSelected={isSelected}
+                                                  onSelect={onSelect}
+                                                />
+                                          )
+                                        }
+                                      )
+                                    }
+                                  </div>
+                                )
+                              }
+                            </ContactsHandler>
+                          )
+                        }
+                      </InitData>
+                    </div>
+                  )
+                }
+              </RenderPropHOC.ARRAY>  
+
+            </FileContextProvider>  
+
     </Workarea.main>
   )
 }
